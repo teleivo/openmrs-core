599f750c675fe579a2f46207137056f108976d58
teleivo <mrteleivo@gmail.com>
2017-03-16 13:49:30 +0100

    return if level is blank

--- api/src/main/java/org/openmrs/util/OpenmrsLoggingConfigurator.java
+++ api/src/main/java/org/openmrs/util/OpenmrsLoggingConfigurator.java
@@ -2,12 +2,16 @@ package org.openmrs.util;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.core.config.Configurator;
 
 /**
  * Configures the logging implementation OpenMRS uses i.e. how OpenMRS logs.
  */
 public final class OpenmrsLoggingConfigurator {
+	
+	private static final Logger log = LogManager.getLogger(OpenmrsLoggingConfigurator.class);
 	
 	private OpenmrsLoggingConfigurator() {
 		// This constructor will not be invoked, this is a utility class
@@ -22,14 +26,15 @@ public final class OpenmrsLoggingConfigurator {
 	 */
 	public static void applyLogLevel(String loggerName, String level) {
 		
-		String logger = defaultLoggerNameIfBlank(loggerName);
 		if (StringUtils.isBlank(level)) {
+			log.warn("Trying set invalid log level '{}'. Valid values are trace, debug, info, warn, error or fatal", level);
 			return;
 		}
+		String logger = defaultLoggerNameIfBlank(loggerName);
 		Level logLevel = Level.toLevel(level);
 		Configurator.setLevel(logger, logLevel);
 	}
-
+	
 	private static String defaultLoggerNameIfBlank(String loggerName) {
 		String logger;
 		if (StringUtils.isBlank(loggerName)) {
