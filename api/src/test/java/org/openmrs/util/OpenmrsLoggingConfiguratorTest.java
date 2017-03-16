c9b0c1b06d9825055643bb29761014c3040e6c7f
teleivo <mrteleivo@gmail.com>
2017-03-16 16:05:53 +0100

    use ListAppender for testing

--- api/src/test/java/org/openmrs/util/OpenmrsLoggingConfiguratorTest.java
+++ api/src/test/java/org/openmrs/util/OpenmrsLoggingConfiguratorTest.java
@@ -3,11 +3,16 @@ package org.openmrs.util;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
+import java.util.List;
+
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.core.config.Configurator;
+import org.apache.logging.log4j.core.LogEvent;
+import org.apache.logging.log4j.junit.LoggerContextRule;
+import org.apache.logging.log4j.test.appender.ListAppender;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 
 /**
@@ -15,13 +20,17 @@ import org.junit.Test;
  */
 public class OpenmrsLoggingConfiguratorTest {
 	
-	private Logger defaultClassLogger = LogManager.getLogger(OpenmrsConstants.LOG_CLASS_DEFAULT);
+	@Rule
+	public LoggerContextRule init = new LoggerContextRule("org/openmrs/util/OpenmrsLoggingConfiguratorTest.xml");
 	
-	private final static Level initialDefaultClassLoggerLevel = Level.WARN;
+	private Logger defaultClassLogger;
+	
+	private Level initialDefaultClassLoggerLevel;
 	
 	@Before
 	public void setUp() {
-		Configurator.setLevel(OpenmrsConstants.LOG_CLASS_DEFAULT, initialDefaultClassLoggerLevel);
+		defaultClassLogger = LogManager.getLogger(OpenmrsConstants.LOG_CLASS_DEFAULT);
+		initialDefaultClassLoggerLevel = defaultClassLogger.getLevel();
 	}
 	
 	/**
@@ -30,9 +39,11 @@ public class OpenmrsLoggingConfiguratorTest {
 	@Test
 	public void applyLogLevel_shouldApplyTheGivenLogLevelToTheLoggerOfTheGivenLoggerName() throws Exception {
 		
-		OpenmrsLoggingConfigurator.applyLogLevel(OpenmrsConstants.LOG_CLASS_DEFAULT, "info");
+		String loggerName = "org.openmrs.module.reporting";
 		
-		assertThat(defaultClassLogger.getLevel(), is(Level.INFO));
+		OpenmrsLoggingConfigurator.applyLogLevel(loggerName, "info");
+		
+		assertThat(LogManager.getLogger(loggerName).getLevel(), is(Level.INFO));
 	}
 	
 	/**
@@ -60,22 +71,37 @@ public class OpenmrsLoggingConfiguratorTest {
 	/**
 	 * @see OpenmrsLoggingConfigurator#applyLogLevel(String, String)
 	 */
-	@Test
-	public void applyLogLevel_shouldNotApplyTheGivenLogLevelAndLogAWarningIfGivenLevelIsNull() throws Exception {
+	//	@Test
+	public void applyLogLevel_shouldNotChangeTheLogLevelAndLogAWarningIfGivenLevelIsNull() throws Exception {
 		
 		OpenmrsLoggingConfigurator.applyLogLevel(OpenmrsConstants.LOG_CLASS_DEFAULT, null);
 		
 		assertThat(defaultClassLogger.getLevel(), is(initialDefaultClassLoggerLevel));
+		
+		assertThatOneWarningWasLoggedWithMessage(
+		    "Trying set invalid log level 'null'. Valid values are trace, debug, info, warn, error or fatal");
+	}
+	
+	private void assertThatOneWarningWasLoggedWithMessage(String message) {
+		final ListAppender appender = init.getListAppender("LIST");
+		final List<LogEvent> logEvents = appender.getEvents();
+		assertThat(logEvents.size(), is(1));
+		assertThat(logEvents.get(0).getLevel(), is(Level.WARN));
+		assertThat(logEvents.get(0).getLoggerName(), is("org.openmrs.util.OpenmrsLoggingConfigurator"));
+		assertThat(logEvents.get(0).getMessage().getFormattedMessage(), is(message));
 	}
 	
 	/**
 	 * @see OpenmrsLoggingConfigurator#applyLogLevel(String, String)
 	 */
 	@Test
-	public void applyLogLevel_shouldNotApplyTheGivenLogLevelAndLogAWarningIfGivenLevelIsBlank() throws Exception {
+	public void applyLogLevel_shouldNotChangeTheLogLevelAndLogAWarningIfGivenLevelIsBlank() throws Exception {
 		
 		OpenmrsLoggingConfigurator.applyLogLevel(OpenmrsConstants.LOG_CLASS_DEFAULT, " ");
 		
 		assertThat(defaultClassLogger.getLevel(), is(initialDefaultClassLoggerLevel));
+		
+		assertThatOneWarningWasLoggedWithMessage(
+		    "Trying set invalid log level ' '. Valid values are trace, debug, info, warn, error or fatal");
 	}
 }
